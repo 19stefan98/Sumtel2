d4ee6de741c5d8a1a85062d97f95d1f56f2eed04
Степан Митрофанов <stepan.mitrofanov.2015@mail.ru>
2018-02-12 10:58:19 +0400

    поиск

---
+++ SumtelSearch/Sh.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace SumtelSearch
+{
+    public class Sh
+    {
+        public IWebDriver driver { get; set; }
+        public int Internet { get; set; }
+        public Sh(IWebDriver driver)
+
+        {
+            this.driver = driver;
+        }
+
+        public void Action()
+        {
+            TimeSpan timeout = new TimeSpan(00, 00, 10);
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("http://itech:itech@voronezh.sumtel.itech-test.ru");
+
+            var search = (new WebDriverWait(driver, timeout)).Until(ExpectedConditions.ElementIsVisible(By.Name("q")));
+            search.SendKeys("Интернет");
+            search.Submit();
+            var root = (new WebDriverWait(driver, timeout)).Until(ExpectedConditions.PresenceOfAllElementsLocatedBy(By.ClassName("sr-item")));
+            Internet = Convert.ToInt32(root.Count);
+        }
+    }
+}
--- SumtelSearch/UnitTest1.cs
+++ SumtelSearch/UnitTest1.cs
@@ -1,14 +1,47 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
 
 namespace SumtelSearch
 {
-    [TestClass]
+    [TestFixture]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        IWebDriver driver;
+
+        //[OneTimeSetUp] // вызывается перед началом запуска всех тестов
+        //public void OneTimeSetUp()
+        //{
+
+        //}
+
+        //[OneTimeTearDown] //вызывается после завершения всех тестов
+        //public void OneTimeTearDown()
+        //{
+
+        //}
+
+        [SetUp] // вызывается перед каждым тестом
+        public void SetUp()
         {
+            driver = new ChromeDriver();
+        }
+
+        [TearDown] // вызывается после каждого теста
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+
+        [Test]
+        public void Search()
+        {
+
+
+            Sh avto = new Sh(driver);
+            avto.Action();
+
+            Assert.AreNotEqual(avto.Internet, 0);
         }
     }
 }
